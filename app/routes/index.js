004b30c7dcc861c8e8b82f9cee8d1297d9ef6bc8
Richard Saldivar <rsaldiva@gmail.com>
2016-06-15 01:36:46 +0000

    update login and profile pages

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,64 +2,68 @@
 
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
-var BookHandler  = require(path + '/app/controllers/bookHandler.server.js');
-module.exports = function (app, passport) {
+var BookHandler = require(path + '/app/controllers/bookHandler.server.js');
+module.exports = function(app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
 
-	var clickHandler = new ClickHandler();
-	var bookHandler = new BookHandler();
+      res.send(JSON.stringify({
+        error: "you are not logged in."
+      }));
+    }
+  }
 
-	app.route('/')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-	app.route('/search')
-	.get(function (req, res) {
-			res.sendFile(path + '/public/search.html');
-		});
-	
-	app.route('/testing')
-		.get(bookHandler.searchBooks)
+  var clickHandler = new ClickHandler();
+  var bookHandler = new BookHandler();
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+  app.route('/')
+    .get(function(req, res) {
+      res.sendFile(path + '/public/index.html');
+    });
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+  app.route('/search')
+    .get(isLoggedIn, function(req, res) {
+      res.sendFile(path + '/public/search.html');
+    });
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
+  app.route('/testing')
+    .get(bookHandler.searchBooks)
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+  app.route('/login')
+    .get(function(req, res) {
+      res.sendFile(path + '/public/login.html');
+    });
 
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
+  app.route('/logout')
+    .get(function(req, res) {
+      req.logout();
+      res.redirect('/login');
+    });
 
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
+  app.route('/profile')
+    .get(isLoggedIn, function(req, res) {
+      res.sendFile(path + '/public/profile.html');
+    });
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+  app.route('/api/:id')
+    .get(isLoggedIn, function(req, res) {
+      res.json(req.user.github);
+    });
+
+  app.route('/auth/github')
+    .get(passport.authenticate('github'));
+
+  app.route('/auth/github/callback')
+    .get(passport.authenticate('github', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    }));
+
+  app.route('/api/:id/clicks')
+    .get(isLoggedIn, clickHandler.getClicks)
+    .post(isLoggedIn, clickHandler.addClick)
+    .delete(isLoggedIn, clickHandler.resetClicks);
 };
